a281b8a57b0870a665bbe1c8138af75d6058e475
JIN <dlwlsgud98@naver.com>
2024-06-18 18:14:16 +0900

    Initial commit

---
+++ src/test/java/bank/bangbank/BangBankApplicationTests.java
@@ -0,0 +1,13 @@
+package bank.bangbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
